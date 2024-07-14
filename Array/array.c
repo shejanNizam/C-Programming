221abe060f878b3ebae44c7b529a84ab491b06c0
shejanNizam <nizam.shejan11@gmail.com>
2024-07-14 16:20:05 +0600

    array problem solved

--- Array/array.c
+++ Array/array.c
@@ -2,18 +2,36 @@
 
 int main()
 {
-    int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+    // Q1 --> solve
+    /*
+        int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
 
-    float sum = 0, avrage;
+        float sum = 0, avrage;
+        int length = sizeof(ages) / sizeof(ages[0]);
+
+        for (int i = 0; i < length; i++)
+        {
+            sum += ages[i];
+        }
+        avrage = sum / length;
+
+        printf("Avrage: %f", avrage);
+    */
+
+    // Q2 --> solve
+    int ages[] = {20, 22, 18, 35, 48, 16, 87, 70};
+
     int length = sizeof(ages) / sizeof(ages[0]);
+    int lowestAge = ages[0];
 
     for (int i = 0; i < length; i++)
     {
-        sum += ages[i];
+        if (lowestAge > ages[i])
+        {
+            lowestAge = ages[i];
+        }
     }
-    avrage = sum / length;
-
-    printf("Avrage: %f", avrage);
+    printf("\n Lowest Age is: %d \n", lowestAge);
 
     return 0;
 };
